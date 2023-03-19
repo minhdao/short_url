03d430b497dbf32c724ccb236f923704cfd6ad29
minh <minh@ubuntu.myguest.virtualbox.org>
2023-03-19 16:21:54 +0800

    Create controller and routes for creating a user with username, email and password

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: { username: @user.username, email: @user.email, created_at: @user.created_at, updated_at: @user.updated_at }, status: :created
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :email, :password)
+  end
+end
