babe69cd41bf70b8a9e8d55ddaa9ce32941b3e30
minh <minh@ubuntu.myguest.virtualbox.org>
2023-03-19 22:28:55 +0700

    Create short link service to handle logics for creating short link

---
+++ app/services/short_link_service.rb
@@ -0,0 +1,35 @@
+class ShortLinkService
+
+  # str_time is to allow the choice of producing the same output or
+  # generating a new output for each original_link and uuid
+  def self.hash(original_link, uuid, str_time = nil)
+    str_time ||= gen_str_time
+    Digest::SHA1.hexdigest(original_link + uuid + str_time)
+  end
+
+  def self.slice(hashed_link, beginning, ending)
+    hashed_link[beginning..ending]
+  end
+
+  def self.build(id)
+    "#{protocol}://#{domain}.#{top_level_domain}/#{id}"
+  end
+
+  private
+
+  def self.gen_str_time
+    Datetime.current.strftime("%H%M%S%L%N")
+  end
+
+  def self.protocol
+    'http'
+  end
+
+  def self.domain
+    'short'
+  end
+
+  def self.top_level_domain
+    'er'
+  end
+end
