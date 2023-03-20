830a1991328e11561a5936891c410a73acb09386
minh <minh@ubuntu.myguest.virtualbox.org>
2023-03-20 14:47:31 +0700

    Add method to imcoming request based on JwT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+
+  def authorize_request
+    auth_header = request.headers['Authorization']
+    token = auth_header.split(' ').last
+
+    begin
+      @decoded = Security::JsonWebToken.decode(token)
+      @decoded_user = User.find_by_username!(@decoded[:username])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError, JWT::ExpiredSignature => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
 end
